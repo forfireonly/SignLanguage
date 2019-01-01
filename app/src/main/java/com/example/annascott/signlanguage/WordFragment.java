f2847d424b96a4c726a8faff9b9a88d8f159b2e4
Anna Scott <workwithpurpose@yahoo.com>
2019-01-01 16:25:55 +0300

    Loading signs on search

--- app/src/main/java/com/example/annascott/signlanguage/WordFragment.java
+++ app/src/main/java/com/example/annascott/signlanguage/WordFragment.java
@@ -6,19 +6,60 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class WordFragment extends Fragment {
+    String word;
+    Button searchButton1;
+    View peaceView;
+    EditText word_to_lookup;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.word, container, false);
-        Button searchButton = (Button) rootView.findViewById(R.id.search_button);
-        final View peaceView = (View) rootView.findViewById(R.id.peace_view);
-        searchButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                // your handler code here
-                peaceView.setVisibility(View.VISIBLE);
+        final View rootView = inflater.inflate(R.layout.word, container, false);
+        searchButton1 = (Button) rootView.findViewById(R.id.search_button_1);
+        peaceView = (View) rootView.findViewById(R.id.peace_view);
+        word_to_lookup = (EditText) rootView.findViewById(R.id.word);
+        searchButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                word = word_to_lookup.getText().toString().trim();
+                if (word.equalsIgnoreCase("zero")){
+                    peaceView.setBackgroundResource(R.drawable.zero);
+                }
+                if (word.equalsIgnoreCase("one")){
+                    peaceView.setBackgroundResource(R.drawable.one);
+                }
+                if (word.equalsIgnoreCase("two")){
+                    peaceView.setBackgroundResource(R.drawable.two);
+                }
+                if (word.equalsIgnoreCase("three")){
+                    peaceView.setBackgroundResource(R.drawable.three);
+                }
+                if (word.equalsIgnoreCase("four")){
+                    peaceView.setBackgroundResource(R.drawable.four);
+                }
+                if (word.equalsIgnoreCase("five")){
+                    peaceView.setBackgroundResource(R.drawable.zero);
+                }
+                if (word.equalsIgnoreCase("six")){
+                    peaceView.setBackgroundResource(R.drawable.five);
+                }
+                if (word.equalsIgnoreCase("nine")){
+                    peaceView.setBackgroundResource(R.drawable.nine);
+                }
+                if (word.equalsIgnoreCase("seven")){
+                    peaceView.setBackgroundResource(R.drawable.seven);
+                }
+                if (word.equalsIgnoreCase("eight")){
+                    peaceView.setBackgroundResource(R.drawable.eight);
+                }
+                else {
+                    peaceView.setBackgroundResource(R.drawable.spelling_check);
+                }
             }
         });
+
+
     return rootView;} }
