d0049996e27bf0f3bf4c60fd8710475678cbfc51
samuel.cahyawijaya@gmail.com <samuel.cahyawijaya@gmail.com>
2019-01-05 22:44:35 +0700

    add tflite model & dependencies, add ImageClassifier.java - for tensorflowlite flow, update manifest - remove camera permission (this is to prevent security exception for API 23+)

---
+++ app/src/main/java/com/example/annascott/signlanguage/ImageClassifier.java
@@ -0,0 +1,397 @@
+/* Copyright 2017 The TensorFlow Authors. All Rights Reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+package com.example.annascott.signlanguage;
+
+import android.app.Activity;
+import android.content.res.AssetFileDescriptor;
+import android.graphics.Bitmap;
+import android.os.SystemClock;
+import android.text.SpannableString;
+import android.text.SpannableStringBuilder;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.RelativeSizeSpan;
+import android.util.Log;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+import org.tensorflow.lite.Interpreter;
+
+/**
+ * Classifies images with Tensorflow Lite.
+ */
+public class ImageClassifier {
+    private float[][] labelProbArray = null;
+
+    // Display preferences
+    private static final float GOOD_PROB_THRESHOLD = 0.3f;
+    private static final int SMALL_COLOR = 0xffddaa88;
+
+    /**
+     * The net requires additional normalization of the used input.
+     */
+    private static final float IMAGE_MEAN = 127.5f;
+    private static final float IMAGE_STD = 127.5f;
+
+    /** Tag for the {@link Log}. */
+    private static final String TAG = "Sign Language";
+
+    /** Number of results to show in the UI. */
+    private static final int RESULTS_TO_SHOW = 10;
+
+    /** Dimensions of inputs. */
+    private static final int DIM_BATCH_SIZE = 1;
+
+    private static final int DIM_PIXEL_SIZE = 1;
+
+    /** Preallocated buffers for storing image data in. */
+    private int[] intValues = new int[getImageSizeX() * getImageSizeY()];
+
+    /** Options for configuring the Interpreter. */
+    private final Interpreter.Options tfliteOptions = new Interpreter.Options();
+
+    /** The loaded TensorFlow Lite model. */
+    private MappedByteBuffer tfliteModel;
+
+    /** An instance of the driver class to run model inference with Tensorflow Lite. */
+    protected Interpreter tflite;
+
+    /** Labels corresponding to the output of the vision model. */
+    private List<String> labelList;
+
+    /** A ByteBuffer to hold image data, to be feed into Tensorflow Lite as inputs. */
+    protected ByteBuffer imgData = null;
+
+    /** multi-stage low pass filter * */
+    private float[][] filterLabelProbArray = null;
+
+    private static final int FILTER_STAGES = 3;
+    private static final float FILTER_FACTOR = 0.4f;
+
+    private PriorityQueue<Map.Entry<String, Float>> sortedLabels =
+            new PriorityQueue<>(
+                    RESULTS_TO_SHOW,
+                    new Comparator<Map.Entry<String, Float>>() {
+                        @Override
+                        public int compare(Map.Entry<String, Float> o1, Map.Entry<String, Float> o2) {
+                            return (o1.getValue()).compareTo(o2.getValue());
+                        }
+                    });
+
+    public String prediction = null;
+
+    /** holds a gpu delegate */
+//    Delegate gpuDelegate = null;
+
+    /** Initializes an {@code ImageClassifier}. */
+    public ImageClassifier(Activity activity) throws IOException {
+        tfliteModel = loadModelFile(activity);
+        tflite = new Interpreter(tfliteModel, tfliteOptions);
+        labelList = loadLabelList(activity);
+        labelProbArray = new float[1][getNumLabels()];
+        imgData = ByteBuffer.allocateDirect(
+                        DIM_BATCH_SIZE
+                                * getImageSizeX()
+                                * getImageSizeY()
+                                * DIM_PIXEL_SIZE
+                                * getNumBytesPerChannel());
+        imgData.order(ByteOrder.nativeOrder());
+        filterLabelProbArray = new float[FILTER_STAGES][getNumLabels()];
+        Log.e(TAG, "Created a Tensorflow Lite Image Classifier.");
+    }
+
+    /** Classifies a frame from the preview stream. */
+    public void classifyFrame(Bitmap bitmap) {
+        if (tflite == null) {
+            Log.e(TAG, "Image classifier has not been initialized; Skipped.");
+        }
+
+        Log.e(TAG,"Convert");
+        convertBitmapToByteBuffer(bitmap);
+        // Here's where the magic happens!!!
+        Log.e(TAG,"Run Inference");
+        long startTime = SystemClock.uptimeMillis();
+        runInference();
+        long endTime = SystemClock.uptimeMillis();
+        Log.e(TAG, "Timecost to run model inference: " + Long.toString(endTime - startTime));
+
+        // Smooth the results across frames.
+        applyFilter();
+        Log.e(TAG, "End Apply Filter");
+
+        // get the results.
+        getTopKLabels();
+        long duration = endTime - startTime;
+    }
+
+    private void applyFilter() {
+        int numLabels = getNumLabels();
+
+        // Low pass filter `labelProbArray` into the first stage of the filter.
+        for (int j = 0; j < numLabels; ++j) {
+            filterLabelProbArray[0][j] +=
+                    FILTER_FACTOR * (getProbability(j) - filterLabelProbArray[0][j]);
+        }
+        // Low pass filter each stage into the next.
+        for (int i = 1; i < FILTER_STAGES; ++i) {
+            for (int j = 0; j < numLabels; ++j) {
+                filterLabelProbArray[i][j] +=
+                        FILTER_FACTOR * (filterLabelProbArray[i - 1][j] - filterLabelProbArray[i][j]);
+            }
+        }
+
+        // Copy the last stage filter output back to `labelProbArray`.
+        for (int j = 0; j < numLabels; ++j) {
+            setProbability(j, filterLabelProbArray[FILTER_STAGES - 1][j]);
+        }
+    }
+
+    private void recreateInterpreter() {
+        if (tflite != null) {
+            tflite.close();
+            // TODO(b/120679982)
+            // gpuDelegate.close();
+            tflite = new Interpreter(tfliteModel, tfliteOptions);
+        }
+    }
+
+    public void useGpu() {
+//        if (gpuDelegate == null && GpuDelegateHelper.isGpuDelegateAvailable()) {
+//            gpuDelegate = GpuDelegateHelper.createGpuDelegate();
+//            tfliteOptions.addDelegate(gpuDelegate);
+//            recreateInterpreter();
+//        }
+    }
+
+    public void useCPU() {
+        tfliteOptions.setUseNNAPI(false);
+        recreateInterpreter();
+    }
+
+    public void useNNAPI() {
+        tfliteOptions.setUseNNAPI(true);
+        recreateInterpreter();
+    }
+
+    public void setNumThreads(int numThreads) {
+        tfliteOptions.setNumThreads(numThreads);
+        recreateInterpreter();
+    }
+
+    /** Closes tflite to release resources. */
+    public void close() {
+        tflite.close();
+        tflite = null;
+        tfliteModel = null;
+    }
+
+    /** Reads label list from Assets. */
+    private List<String> loadLabelList(Activity activity) throws IOException {
+        List<String> labels = new ArrayList<String>();
+
+        labels.add("NINE");
+        labels.add("ZERO");
+        labels.add("SEVEN");
+        labels.add("SIX");
+        labels.add("ONE");
+        labels.add("EIGHT");
+        labels.add("FOUR");
+        labels.add("THREE");
+        labels.add("TWO");
+        labels.add("FIVE");
+
+        return labels;
+    }
+
+    /** Memory-map the model file in Assets. */
+    private MappedByteBuffer loadModelFile(Activity activity) throws IOException {
+        AssetFileDescriptor fileDescriptor = activity.getAssets().openFd(getModelPath());
+        FileInputStream inputStream = new FileInputStream(fileDescriptor.getFileDescriptor());
+        FileChannel fileChannel = inputStream.getChannel();
+        long startOffset = fileDescriptor.getStartOffset();
+        long declaredLength = fileDescriptor.getDeclaredLength();
+        return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength);
+    }
+
+    /** Writes Image data into a {@code ByteBuffer}. */
+    private void convertBitmapToByteBuffer(Bitmap bitmap) {
+        Log.e(TAG, "convertBitmapToByteBuffer " + (imgData == null));
+        if (imgData == null) {
+            return;
+        }
+        imgData.rewind();
+        Log.e(TAG, "convertBitmapToByteBuffer A " + bitmap.getWidth() + " & " + bitmap.getHeight());
+        bitmap.getPixels(intValues, 0, bitmap.getWidth(), 0, 0, bitmap.getWidth(), bitmap.getHeight());
+        // Convert the image to floating point.
+        Log.e(TAG, "convertBitmapToByteBuffer B");
+        int pixel = 0;
+        long startTime = SystemClock.uptimeMillis();
+        for (int i = 0; i < getImageSizeX(); ++i) {
+            for (int j = 0; j < getImageSizeY(); ++j) {
+                final int val = intValues[pixel++];
+                addPixelValue(val);
+            }
+        }
+        Log.e(TAG, "convertBitmapToByteBuffer C");
+        long endTime = SystemClock.uptimeMillis();
+        Log.e(TAG, "Timecost to put values into ByteBuffer: " + Long.toString(endTime - startTime));
+    }
+
+    /** Get top-K labels */
+    private void getTopKLabels() {
+        sortedLabels.clear();
+        for (int i = 0; i < getNumLabels(); i++) {
+            sortedLabels.add(
+                    new AbstractMap.SimpleEntry<>(labelList.get(i), getNormalizedProbability(i)));
+            if (sortedLabels.size() > RESULTS_TO_SHOW) {
+                sortedLabels.poll();
+            }
+        }
+
+        Log.e(TAG, "RetrieveTopK");
+        final int size = sortedLabels.size();
+        for (int i = 0; i < size; i++) {
+            Map.Entry<String, Float> label = sortedLabels.poll();
+            String key = label.getKey();
+            float val = label.getValue();
+            Log.e(TAG, String.format("%s: %4.5f\n", key, val));
+            prediction = key;
+        }
+    }
+
+    /**
+     * Get the name of the model file stored in Assets.
+     *
+     * @return
+     */
+    protected String getModelPath() {
+        // you can download this file from
+        // https://storage.googleapis.com/download.tensorflow.org/models/tflite/inception_v3_slim_2016_android_2017_11_10.zip
+        return "sign_lang_net.tflite";
+    }
+
+    /**
+     * Get the name of the label file stored in Assets.
+     *
+     * @return
+     */
+    protected String getLabelPath() {
+        return "labels.json";
+    }
+    /**
+     * Get the image size along the x axis.
+     *
+     * @return
+     */
+    protected int getImageSizeX() {
+        return 64;
+    }
+
+    /**
+     * Get the image size along the y axis.
+     *
+     * @return
+     */
+    protected int getImageSizeY() {
+        return 64;
+    }
+
+    /**
+     * Get the number of bytes that is used to store a single color channel value.
+     *
+     * @return
+     */
+    protected int getNumBytesPerChannel() {
+        // a 32bit float value requires 4 bytes
+        return 4;
+    }
+
+    /**
+     * Add pixelValue to byteBuffer.
+     *
+     * @param pixelValue
+     */
+    protected void addPixelValue(int pixelValue){
+        imgData.putFloat(
+                (float) (
+                    (0.21 * ((((pixelValue >> 16) & 0xFF) - IMAGE_MEAN) / IMAGE_STD)) +
+                    (0.72 * ((((pixelValue >> 8) & 0xFF) - IMAGE_MEAN) / IMAGE_STD)) +
+                    (0.07 * (((pixelValue & 0xFF) - IMAGE_MEAN) / IMAGE_STD))
+                )
+        );
+    }
+
+    /**
+     * Read the probability value for the specified label This is either the original value as it was
+     * read from the net's output or the updated value after the filter was applied.
+     *
+     * @param labelIndex
+     * @return
+     */
+    protected float getProbability(int labelIndex){
+        return labelProbArray[0][labelIndex];
+    }
+
+    /**
+     * Set the probability value for the specified label.
+     *
+     * @param labelIndex
+     * @param value
+     */
+    protected void setProbability(int labelIndex, Number value){
+        labelProbArray[0][labelIndex] = value.floatValue();
+    }
+
+    /**
+     * Get the normalized probability value for the specified label. This is the final value as it
+     * will be shown to the user.
+     *
+     * @return
+     */
+    protected float getNormalizedProbability(int labelIndex) {
+        return getProbability(labelIndex);
+    }
+
+    /**
+     * Run inference using the prepared input in {@link #imgData}. Afterwards, the result will be
+     * provided by getProbability().
+     *
+     * <p>This additional method is necessary, because we don't have a common base for different
+     * primitive data types.
+     */
+    protected void runInference(){
+        tflite.run(imgData, labelProbArray);
+    }
+
+    /**
+     * Get the total number of labels.
+     *
+     * @return
+     */
+    protected int getNumLabels() {
+        return labelList.size();
+    }
+}
--- app/src/main/java/com/example/annascott/signlanguage/SignFragment.java
+++ app/src/main/java/com/example/annascott/signlanguage/SignFragment.java
@@ -31,10 +31,12 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 
-import org.tensorflow.contrib.android.TensorFlowInferenceInterface;
+//import org.tensorflow.contrib.android.TensorFlowInferenceInterface;
+import org.tensorflow.lite.Interpreter;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 
@@ -51,16 +53,18 @@ public class SignFragment extends Fragment {
 
     //PATH TO OUR MODEL FILE AND NAMES OF THE INPUT AND OUTPUT NODES
     private String MODEL_PATH = "file:///android_asset/sign_lang_net.pb";
-    private String INPUT_NAME = "0:0";
-    private String OUTPUT_NAME = "add_16:0";
-    private TensorFlowInferenceInterface tf;
+    private String INPUT_NAME = "0";
+    private String OUTPUT_NAME = "add_13:0";
+
+    //    private TensorFlowInferenceInterface tf;
+    private ImageClassifier classifier;
 
     Bitmap bitmap;
 
     //ARRAY TO HOLD THE PREDICTIONS AND FLOAT VALUES TO HOLD THE IMAGE DATA
     float[] PREDICTIONS = new float[1000];
     private float[] floatValues;
-    private int[] INPUT_SIZE = {224,224,3};
+    private int[] INPUT_SIZE = {64,64,1};
 
     ImageView imageView;
     TextView resultView;
@@ -73,13 +77,17 @@ public class SignFragment extends Fragment {
         final View rootView = inflater.inflate(R.layout.sign,
                 container, false);
 
-
-
         resultView = (TextView) rootView.findViewById(R.id.class_name);
 
-        tf = new TensorFlowInferenceInterface(getContext().getAssets(),MODEL_PATH);
+        // tf = new TensorFlowInferenceInterface(getContext().getAssets(),MODEL_PATH);
+        try {
+            classifier = new ImageClassifier(this.getActivity());
+            classifier.useCPU();
+        } catch (IOException e) {
+            Log.e("Sign Language","FAILED TO INIT MODEL " + e.getMessage());
+        }
 
-       // progressBar = Snackbar.make(imageView,"PROCESSING IMAGE",Snackbar.LENGTH_INDEFINITE);
+        // progressBar = Snackbar.make(imageView,"PROCESSING IMAGE",Snackbar.LENGTH_INDEFINITE);
 
 
         button = (Button) rootView.findViewById(R.id.camera_button);
@@ -174,7 +182,6 @@ public class SignFragment extends Fragment {
     @SuppressLint("StaticFieldLeak")
     public void predict(final Bitmap bitmap){
 
-
         //Runs inference in background thread
         new AsyncTask<Integer,Integer,Integer>(){
 
@@ -182,61 +189,64 @@ public class SignFragment extends Fragment {
 
             protected Integer doInBackground(Integer ...params){
 
+                Bitmap resized_image = ImageUtils.processBitmap(bitmap,64);
+                classifier.classifyFrame(resized_image);
+                Log.e("Sign Language", "PREDICTED LABEL : " + classifier.prediction);
+
                 //Resize the image into 224 x 224
-                Bitmap resized_image = ImageUtils.processBitmap(bitmap,224);
+                // Log.e("Sign Language","POKEMON");
+                // Log.e("Sign Language","WKWKWKWK");
 
                 //Normalize the pixels
-                floatValues = ImageUtils.normalizeBitmap(resized_image,224,127.5f,1.0f);
+                // floatValues = ImageUtils.normalizeBitmap(resized_image,224,127.5f,1.0f);
 
                 //Pass input into the tensorflow
-                tf.feed(INPUT_NAME,floatValues,1,3,224,224);
+//                tf.feed(INPUT_NAME,floatValues,1,3,224,224);
 
                // Log.w("myApp", "input");
                 //compute predictions
-                tf.run(new String[]{OUTPUT_NAME});
+
+//                tf.run(new String[]{OUTPUT_NAME});
 
                 //Log.w("myApp", "output");
 
                 //copy the output into the PREDICTIONS array
-                tf.fetch(OUTPUT_NAME,PREDICTIONS);
+//                tf.fetch(OUTPUT_NAME,PREDICTIONS);
 
                // Log.w("myApp", "predictions");
                 //Obtained highest prediction
-                Object[] results = argmax(PREDICTIONS);
+                // Object[] results = argmax(PREDICTIONS);
 
 
-                int class_index = (Integer) results[0];
-                float confidence = (Float) results[1];
+                // int class_index = (Integer) results[0];
+                // float confidence = (Float) results[1];
 
-
-                try{
-
-                    final String conf = String.valueOf(confidence * 100).substring(0,5);
-
-
-
-                    //Convert predicted class index into actual label name
-                    final String label = ImageUtils.getLabel(getContext().getAssets().open("labels.json"),class_index);
-
-
-
-                    //Display result on UI
-                    getActivity().runOnUiThread(new Runnable() {
-                       @Override
-                        public void run() {
-
-                            progressBar.dismiss();
-                            resultView.setText(label + " : " + conf + "%");
-
-                        }
-                    });
-
-                }
-
-                catch (Exception e){
-
-
-               }
+//                try{
+//
+//                    final String conf = String.valueOf(confidence * 100).substring(0,5);
+//
+//
+//
+//                    //Convert predicted class index into actual label name
+//                    final String label = ImageUtils.getLabel(getContext().getAssets().open("labels.json"),class_index);
+//
+//
+//
+//                    //Display result on UI
+//                    getActivity().runOnUiThread(new Runnable() {
+//                       @Override
+//                        public void run() {
+//
+//                            progressBar.dismiss();
+//                            resultView.setText(label + " : " + conf + "%");
+//
+//                        }
+//                    });
+//
+//                }
+//                catch (Exception e){
+//
+//               }
 
 
                 return 0;
